acb1dbcaf699db4a360d9110b28e04b714e316ec
王易弈 <wang_yiyi@v1040-wn-rt-a-40-2.campus-dynamic.uwaterloo.ca>
2022-07-31 15:46:00 -0400

    added intro to the game

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def main():
-    print(input("what is your name?"))
+import time
+
+
+def intro():
+    print("Once upon a time...")
+    time.sleep(1.5)
+    print("In a world where dwelf lives and dragon flies...")
+    time.sleep(2)
+    print("Once you fell from the sky and opened your eyes... You heard someone whispering:")
+    time.sleep(2)
+    print('"What\'s your name?" says a cat hanging on the tree')
+    time.sleep(2)
+    name = input("(You recall that your name is...)")
+    time.sleep(1)
+    print ("Oh ok so. " + name +", I need you to do me a favor...")
 
 if __name__ == "__main__":
-    main()
+    intro()
